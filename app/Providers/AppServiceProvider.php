ae090456c9bcb04528b233d272addb693b9b5a90
Hamura6 <deidaramen2@gmail.com>
2026-07-07 06:27:10 +0200

    configuracion de los assets para railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
 
         $this->configureTallStackUi();
         $this->configureDefaults();
+        if ($this->app->environment('production')) {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+    }
         require_once app_path('Helpers/NumberHelper.php');
     }
 
